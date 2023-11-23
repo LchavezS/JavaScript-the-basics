b438cadbaa377e64ef5938a79e8213bd5b99e470
Ing.Lenin_ChavezS <chavez.lenin@gmail.com>
2023-11-23 15:50:10 -0600

    Added file JavaScript fundamentals

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log ("Hello World!!!");
+            
+            /*Declare the variables in JS*/
+            
+            var myVariable = "FreeCodeCamp" //Data type string
+            console.log(myVariable);
+            
+            myVariable = 1; //Data type number
+            console.log(myVariable);
+            
+            var myName = "Lenin Chavez"; //Convention used in JS is Cammel case
+            console.log (myName);
+            
+            /*Operators*/
+            var a;
+            var b = 2;
+            a = 10; // = is the asigment operator
+            console.log(a);
+
+            var a = 5;
+            var b = a;
+            console.log(a);
+            console.log(b); 
